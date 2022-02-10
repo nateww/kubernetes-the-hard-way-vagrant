f1c5bcb994ece87fa6e47371667127779a435e9c
Nate Williams <nate@yogotech.com>
2022-02-09 20:58:20 -0700

    Update to the application versions in the latest version of the source repo

--- scripts/versions.bash
+++ scripts/versions.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # https://storage.googleapis.com/kubernetes-release/release/stable.txt
-readonly k8s_version="v1.20.2"
-readonly etcd_version="v3.4.14"
+readonly k8s_version="v1.21.0"
+readonly etcd_version="v3.4.15"
 readonly cfssl_version="1.5.0"
 readonly crio_version="v1.19.1"
 readonly traefik_version="v2.4.0"
